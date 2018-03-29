d9dc43a66d598174968970024c06734d1791251d
askabulkahfi <askabulkahfi1@gmail.com>
2018-03-29 19:25:43 +0800

    membuat atribut dan behavior

--- src/prakteksaya/Bantal.java
+++ src/prakteksaya/Bantal.java
@@ -1,4 +1,12 @@
 package prakteksaya;
 public class Bantal {
+    double panjang;
+    double lebar;
+    
+    void cetakInfo(){
+          System.out.println("Panjang       :"+lebar);
+          System.out.println("Panjang       :"+panjang); 
+    }
+    
     
 }
